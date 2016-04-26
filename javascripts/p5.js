59db0c6b4ce9490224b3c73b7a0915dcfea56d82
Rune Madsen <rune@runemadsen.com>
2016-04-26 16:58:43 -0400

    lazyload done

--- javascripts/p5.js
+++ javascripts/p5.js
@@ -3,7 +3,7 @@
 
 function P5toFig(el) {
 
-  var placeholder, iframe;
+  var loaded = false;
 
   // Grab the size of the sketch
   var code = el.innerHTML;
@@ -13,43 +13,43 @@ function P5toFig(el) {
   var sketchWidth = parseInt(hasCanvas[1]);
   var sketchHeight = parseInt(hasCanvas[2]);
 
-  // Create a placeholder
-  placeholder = document.createElement('div');
-  placeholder.setAttribute('class', 'placeholder');
-  setSize(placeholder);
+  // function to set or update the iframe and sketch size
+  function setSize(iframe, updateSketch) {
 
-  function setSize(box) {
     var ratio = el.parentNode.offsetWidth / sketchWidth;
     var w = Math.floor(sketchWidth * ratio);
     var h = Math.floor(sketchHeight * ratio);
-    box.style.width = w + 'px';
-    box.style.height = h + 'px';
-    if(box.tagName === 'IFRAME') {
+    var ws = w + 'px';
+    var hs = h + 'px';
+
+    if(iframe.style.width !== ws || iframe.style.height !== hs) {
+      iframe.style.width = ws;
+      iframe.style.height = hs;
+    }
+
+    if(updateSketch) {
       var fitCode = code.replace(pattern, 'createCanvas('+w+','+h+');');
-      box.setAttribute('srcdoc', '<html><head><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.4.23/p5.js"></script><style>html,body{ margin:0; padding:0}</style></head><body><script type="text/javascript">' + fitCode + '</script></body></html>');
+      iframe.setAttribute('srcdoc', '<html><head><script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.4.23/p5.js"></script><style>html,body{ margin:0; padding:0}</style></head><body><script type="text/javascript">' + fitCode + '</script></body></html>');
     }
   }
 
-  // add placeholder next to script element
-  el.parentNode.insertBefore(placeholder, el.nextSibling);
+  // Create iframe as placeholder and insert next to script
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('class', 'placeholder');
+  iframe.setAttribute('scrolling', 'no');
+  iframe.setAttribute('sandbox', 'allow-scripts');
+  setSize(iframe);
+  el.parentNode.insertBefore(iframe, el.nextSibling);
 
   // When placeholder is in window, replace with iframe
-  lazyload(placeholder, function() {
-
-    // Create an iframe. We have srcdoc polyfill for older browsers.
-    iframe = document.createElement('iframe');
-    iframe.setAttribute('scrolling', 'no');
-    iframe.setAttribute('sandbox', 'allow-scripts');
-    setSize(iframe);
-
-    // replace placeholder with the real thing.
-    placeholder.parentNode.replaceChild(iframe, placeholder);
-
+  lazyload(iframe, function() {
+    loaded = true;
+    setSize(iframe, true);
   });
 
   // Resize placeholder or iframe when window resizes
   window.addEventListener('resizeEnd', function(e) {
-    setSize(iframe || placeholder);
+    setSize(iframe, loaded);
   });
 }
 
