0fcc7f381450aae05c8b3058ef795f3fe5530625
Rune Madsen <rune@runemadsen.com>
2016-05-27 15:30:26 -0400

    tan ma is done

--- examples/shape/basic-shapes/cone-tan.js
+++ examples/shape/basic-shapes/cone-tan.js
@@ -1,10 +1,10 @@
 function setup() {
   createCanvas(600, 700);
-  background(255);
   strokeWeight(width * 0.007);
+  background(255);
 
   var coneX = width / 2;
-  var coneY = height * 0.4;
+  var coneY = height * 0.38;
   var coneSize = width * 0.4;
 
   // Ellipses
@@ -20,10 +20,25 @@ function setup() {
   }
 
   // Triangles
+  push();
   fill(255);
   translate(0, coneSize * 1.5);
   for(var i = -1; i <= 1; i += 2) {
     triangle(0, 0, i * (coneSize * 0.55), -(coneSize*1.15), -(coneSize * 0.38) * i, -(coneSize*0.8));
+  }
+  pop();
+
+  // Rectangles
+  translate(coneSize * 0.2, -(coneSize*0.8));
+  rotate(radians(30));
+  stroke(255);
+  fill(0);
+  var rectWidth = coneSize * 0.2;
+  var rectHeight = coneSize * 0.15;
+  for(var i = 0; i < 2; i++) {
+    for(var j = 0; j < 5; j++) {
+      rect(i * rectWidth, j * rectHeight, rectWidth, rectHeight);
+    }
   }
 
   noLoop();
