9f2ea429f62b21217927f3389c49666cedf5967e
Rune Madsen <rune@runemadsen.com>
2016-06-20 11:50:14 -0400

    more

--- playground/animation/sketch.js
+++ playground/animation/sketch.js
@@ -1,12 +1,14 @@
 var queue = [];
 var cur = 0;
+var colors = ["#6e97b2", "#ae3c37", "#ecc94a", "#3f8358", "#9372ae"]
 
 // Line
 // --------------------------------------------
 
-function Line(start, stop) {
+function Line(start, stop, col) {
   this.start = start;
   this.stop = stop;
+  this.col = col;
   this.cur = createVector(0, 0);
   var diff = p5.Vector.sub(stop, start);
   this.mag = diff.mag();
@@ -14,15 +16,18 @@ function Line(start, stop) {
 }
 
 Line.prototype = {
+
   display: function() {
-    fill(30);
-    stroke(30);
+
+    fill(this.col);
+    stroke(this.col);
+    strokeWeight(1.5);
+    ellipse(this.start.x, this.start.y, 6, 6);
     line(this.start.x, this.start.y, this.start.x + this.cur.x, this.start.y + this.cur.y);
+
     if(!this.done) {
       this.cur.add(this.speed);
       this.done = this.cur.mag() > this.mag;
-    } else {
-      ellipse(this.stop.x, this.stop.y, 6, 6);
     }
   }
 }
@@ -45,7 +50,8 @@ function draw() {
   if(!last || last.done) {
     queue.push(new Line(
       last ? last.stop.copy() : createVector(random(0, width),random(0, height)),
-      createVector(random(0, width),random(0, height))
+      createVector(random(0, width),random(0, height)),
+      colors[floor(random(colors.length))]
     ));
   }
 
