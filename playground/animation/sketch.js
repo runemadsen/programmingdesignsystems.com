0b52ea6b46b96840c3514a9daf6d4f0af44ac541
Rune Madsen <rune@runemadsen.com>
2016-06-21 12:23:37 -0400

    fixing animation

--- playground/animation/sketch.js
+++ playground/animation/sketch.js
@@ -5,10 +5,8 @@ var r = new Rune({
 });
 
 var queue = [];
-var cur = -1;
+var cur = 0;
 
-// FIGURE OUT WHY DOTS ARE SO FAR IN ADVANCE
-// ADD DOT WHEN LINE IS CUR, NOT ON CREATION
 // USE OFFSET OF SEGMENTS
 // MAKE SURE SEGMENTS ARE SPACED MORE THAN X APART
 // ADD BEZIER CURVES
@@ -20,7 +18,6 @@ function Line(start, stop) {
   this.start = start;
   this.stop = stop;
   this.line = r.line(start.x, start.y, start.x, start.y);
-  this.dot = r.circle(start.x, start.y, 3);
   this.cur = 0;
   this.speed = 2 / stop.sub(start).length();
 }
@@ -44,8 +41,6 @@ function addStyles(o) {
   var color = colors[Math.floor(r.random(colors.length))];
   o.line.stroke(color);
   o.line.strokeWidth(1.5);
-  o.dot.stroke(false);
-  o.dot.fill(color);
 
   // choose line type
   var seed = r.random(1);
@@ -66,40 +61,38 @@ function addStyles(o) {
 
 r.on('update', function() {
 
-  for(var i = 0; i <= cur; i++) {
-    queue[i].update();
+  // If we need to add more things to the queue
+  if(cur >= queue.length - 1) {
+
+    var last = queue[queue.length-1]
+    var start = last ? last.stop.copy() : new Rune.Vector(r.random(0, r.width),r.random(0, r.height));
+    var stop = new Rune.Vector(r.random(0, r.width),r.random(0, r.height));
+
+    // Zig Zag lines
+    var numLines = Math.round(r.random(1, 5));
+    var splits = [];
+    var offsets = [];
+    for(var i = 0; i < numLines; i++) {
+      splits.push(r.random(1));
+      offsets.push(r.random(-100, 100));
+    }
+    splits.sort();
+    for(var i = 0; i < numLines; i++) {
+      var segmentStart = start.lerp(stop, i == 0 ? 0 : splits[i-1]);
+      var segmentStop = start.lerp(stop, i == numLines-1 ? 1 : splits[i]);
+      queue.push(addStyles(new Line(segmentStart, segmentStop)));
+    }
   }
 
-  if(cur == -1 || queue[cur].done) {
-
-    // If we need to add more things to the queue
-    if(cur < queue.length) {
-
-      var last = queue[queue.length-1]
-      var start = last ? last.stop.copy() : new Rune.Vector(r.random(0, r.width),r.random(0, r.height));
-      var stop = new Rune.Vector(r.random(0, r.width),r.random(0, r.height));
-
-      // create just a single line
-      // queue.push(addStyles(new Line(start, stop)));
-
-      // Zig Zag lines
-      var numLines = Math.round(r.random(1, 5));
-      var splits = [];
-      var offsets = [];
-      for(var i = 0; i < numLines; i++) {
-        splits.push(r.random(1));
-        offsets.push(r.random(-100, 100));
-      }
-      splits.sort();
-      for(var i = 0; i < numLines; i++) {
-        var segmentStart = start.lerp(stop, i == 0 ? 0 : splits[i-1]);
-        var segmentStop = start.lerp(stop, i == numLines-1 ? 1 : splits[i]);
-        queue.push(addStyles(new Line(segmentStart, segmentStop)));
-      }
-    }
-
+  if(queue[cur].done) {
+    var curLine = queue[cur].line;
+    r.circle(curLine.state.x2, curLine.state.y2, 3)
+      .fill(curLine.state.stroke)
+      .stroke(false)
     cur++;
   }
+
+  queue[cur].update();
 });
 
 r.play();
