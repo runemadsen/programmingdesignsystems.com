f45b9d17add135568dfc955c8b9836e1abb831a5
Rune Madsen <rune@runemadsen.com>
2016-10-17 11:46:19 -0400

    adding contour example

---
+++ examples/shape/custom-shapes/contour.js
@@ -0,0 +1,26 @@
+function setup()
+{
+  createCanvas(600, 500);
+  background(255, 255, 220);
+
+  var w = width * 0.7;
+  var h = height * 0.7;
+
+  translate((width/2) - (w/2), (height/2) - (h/2));
+
+  fill(30);
+  noStroke();
+  beginShape();
+    vertex(0, 0);
+    vertex(w, 0);
+    vertex(w, h);
+    vertex(0, h);
+    beginContour();
+      vertex(w * 0.3, h * 0.3);
+      vertex(w * 0.5, h * 0.8);
+      vertex(w * 0.8, h * 0.4);
+    endContour();
+  endShape();
+
+  noLoop();
+}
---
+++ examples/shape/custom-shapes/cubic-letter.js
@@ -0,0 +1,19 @@
+function setup()
+{
+  createCanvas(600, 500);
+  background(255, 255, 220);
+
+  var w = width * 0.4;
+  var h = height * 0.5;
+
+  stroke(30);
+  strokeWeight(width*0.1);
+  noFill();
+  translate((width/2) - (w*0.3), (height/2) - (h/2));
+  beginShape();
+    vertex(0, 0);
+    bezierVertex(w, 0, w, h, 0, h);
+  endShape(CLOSE);
+
+  noLoop();
+}
