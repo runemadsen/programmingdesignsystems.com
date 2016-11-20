c54f8321b7377348a131cc9f69c9d444deb040d3
Rune Madsen <rune@runemadsen.com>
2016-11-20 11:50:39 -0500

    codesplit

--- examples/shape/procedural-shapes/star.js
+++ examples/shape/procedural-shapes/star.js
@@ -7,13 +7,18 @@ function setup()
 
   translate(width/2, height/2);
 
-  var radius = 100;  
+  // Set the initial radius to 100 {!1}
+  var radius = 100;
+
   beginShape();
   for(var i = 0; i < 10; i++) {
+
+    // Use the radius in the cos/sin formula {!2}
     var x = cos(radians(i * 36)) * radius;
     var y = sin(radians(i * 36)) * radius;
     vertex(x, y);
 
+    // Before the loop repeats, alternate between a radius of 100 and 50. {!5}
     if(radius == 100) {
       radius = 50;
     } else {
