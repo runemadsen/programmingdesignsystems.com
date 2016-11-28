d4d6fbb627c25e6bc070a599d17d4149c3e422c3
Rune Madsen <rune@runemadsen.com>
2016-11-28 12:11:45 -0500

    done with TOC

--- javascripts/app.jsx
+++ javascripts/app.jsx
@@ -35,6 +35,37 @@ for(var i = 0; i < pres.length; i++) {
   pres[i].firstChild.innerHTML = Prism.highlight(pres[i].firstChild.textContent, Prism.languages.javascript)
 }
 
+// Toggle TOC on click
+var tocToggle = document.getElementById('tocToggle');
+var toc = document.getElementById('tocContainer');
+tocToggle.addEventListener('click', function(e) {
+  e.preventDefault();
+  if(toc.style.display == 'block') {
+    toc.style.display = 'none';
+    ga('send', 'event', 'toc', 'toggle', 'hide');
+  }
+  else {
+    toc.style.display = 'block';
+    ga('send', 'event', 'toc', 'toggle', 'show');
+  }
+});
+
+// Track newsletter clicks
+var newsletter = document.getElementById('newsletter');
+newsletter.addEventListener('click', function() {
+  ga('send', 'event', 'newsletter', 'click');
+});
+
+// Highlight active TOC
+var as = document.querySelectorAll('#tocContainer a[href]');
+for(var i = 0; i < as.length; i++) {
+  // link is the one where link is just index.html
+  var realHref = as[i].getAttribute('href').split('#')[0];
+  if(realHref == 'index.html') {
+    as[i].setAttribute('class', 'color1');
+  }
+}
+
 // Dispatch an event to say that libs are loaded. This makes it
 // possible to have view-specific JS before the loaded libs.
 window.dispatchEvent(new Event('libsLoaded'));
