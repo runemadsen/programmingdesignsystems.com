b8d3c6b884356a1c0082998d324d733481ff48b1
Rune Madsen <rune@runemadsen.com>
2020-03-28 23:08:22 -0400

    lots of more work

--- examples/layout/geometric-composition/thirds-margin-between.js
+++ examples/layout/geometric-composition/thirds-margin-between.js
@@ -3,7 +3,6 @@ function setup()
   createCanvas(450, 600);
 
   var margin = height / 20;
-  var imgWidth = width;
   var allHeight = height - 2 * margin;
   var imgHeight = allHeight / 3;
 
@@ -11,13 +10,13 @@ function setup()
   noStroke();
 
   fill(75, 185, 165);
-  rect(0, 0, imgWidth, imgHeight);
+  rect(0, 0, width, imgHeight);
 
   fill(120, 155, 155);
-  rect(0, imgHeight + margin, imgWidth, imgHeight);
-  
+  rect(0, imgHeight + margin, width, imgHeight);
+
   fill(30, 50, 50);
-  rect(0, 2 * (imgHeight + margin), imgWidth, imgHeight);
+  rect(0, 2 * (imgHeight + margin), width, imgHeight);
 
   noLoop();
 }
--- examples/layout/geometric-composition/thirds.js
+++ examples/layout/geometric-composition/thirds.js
@@ -2,18 +2,17 @@ function setup()
 {
   createCanvas(450, 600);
 
-  var imgWidth = width;
   var imgHeight = height / 3;
 
   noStroke();
   fill(75, 185, 165);
-  rect(0, 0, imgWidth, imgHeight);
+  rect(0, 0, width, imgHeight);
 
   fill(120, 155, 155);
-  rect(0, imgHeight, imgWidth, imgHeight);
+  rect(0, imgHeight, width, imgHeight);
 
   fill(30, 50, 50);
-  rect(0, 2 * imgHeight, imgWidth, imgHeight);
+  rect(0, 2 * imgHeight, width, imgHeight);
 
   noLoop();
 }
